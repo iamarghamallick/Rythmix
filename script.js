6ed7254183c96dd42e4b9c431ba222d2698b22de
Argha Mallick <arghamallick5@gmail.com>
2023-08-20 14:14:24 +0530

    backend hosted on render.com

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const HOST_URL = "http://localhost:8080";
+const HOST_URL = "https://rythmix-backend.onrender.com";
 
 let cardContainer = document.getElementById("card-container");
 let addSongBtn = document.getElementById("addSongBtn");
